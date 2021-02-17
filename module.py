8df352305a059adfe55480b9da3e13c02830f011
Little Cat <toontownlittlecat@gmail.com>
2021-02-17 06:23:49 -0400

    module: Fix BUILD_CONST_KEY_MAP opcode generation
    This ensures that dictionaries are created in the correct order as defined.
    
    Fixes #5

--- module.py
+++ module.py
@@ -551,6 +551,8 @@ class Module(ModuleBase):
             context.end_block()
 
         elif op == BUILD_CONST_KEY_MAP:
+            context.add_decl_once('i', 'int', None, False)
+
             context.begin_block()
 
             context.insert_line('x = POP(); /* keys */')
@@ -562,16 +564,25 @@ class Module(ModuleBase):
             context.insert_handle_error(line, label)
             context.end_block()
 
-            for i in range(oparg):
-                context.insert_line('v = PyTuple_GET_ITEM(x, %d);' % (oparg - i - 1))
-                context.insert_line('w = POP();')
-                context.insert_line('err = PyDict_SetItem(u, v, w);')
-                context.insert_line('Py_DECREF(w);')
-                context.insert_line('if (err != 0)')
-                context.begin_block()
-                context.insert_line('Py_DECREF(u);')
-                context.insert_handle_error(line, label)
-                context.end_block()
+            context.insert_line('for (i = %d; i > 0; i--)' % oparg)
+            context.begin_block()
+            context.insert_line('v = PyTuple_GET_ITEM(x, %d - i);' % oparg)
+            context.insert_line('w = PEEK(0 + i);')
+            context.insert_line('err = PyDict_SetItem(u, v, w);')
+            context.insert_line('if (err != 0)')
+
+            context.begin_block()
+            context.insert_line('Py_DECREF(u);')
+            context.insert_handle_error(line, label)
+            context.end_block()
+
+            context.end_block()
+
+            context.insert_line('for (i = %d; i > 0; i--)' % oparg)
+            context.begin_block()
+            context.insert_line('x = POP();')
+            context.insert_line('Py_DECREF(x);')
+            context.end_block()
 
             context.insert_line('PUSH(u);')
 
